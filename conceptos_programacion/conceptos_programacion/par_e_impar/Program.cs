aa384d3280549cbc453802f97cf2ce88a2d63bab
Diosepulveda015 <diomedesechavarria015@gmail.com>
2025-04-04 11:19:53 -0500

    par e impar

---
+++ conceptos_programacion/conceptos_programacion/par_e_impar/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("digite un numero:");
+        int numero = Convert.ToInt32(Console.ReadLine());
+
+        if (numero % 2 == 0)
+        {
+            Console.WriteLine("El número " + numero + " es par.");
+        }
+        else
+        {
+            Console.WriteLine("El número " + numero + " es impar.");
+        }
+    }
+}
